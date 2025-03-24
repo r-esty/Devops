5b0f18969d73dd13a3f425cf299e00e90bad67e3
r-esty <romeodesty@gmail.com>
2025-03-24 19:58:06 +0000

    Fixed moved files tracking

--- level3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "hero.txt" ]
-then
-    echo "Hero found!"
-else 
-    echo "Hero missing!"
-fi
--- level7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p Size
-touch Size/a.txt Size/b.txt Size/c.txt
-ls -lSr Size/*.txt
