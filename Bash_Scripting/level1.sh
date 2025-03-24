76222dcea1faff9c714525e94b03560b54a5a6d5
Romeo Don Esty <romeodesty@gmail.com>
2025-03-24 19:32:50 +0000

    Update level1.sh

--- Bash_Scripting/level1.sh
+++ Bash_Scripting/level1.sh
@@ -1,1 +1,10 @@
+# Mission: Create a directory named Arena and then inside it, 
+# create three files: warrior.txt, mage.txt, and archer.txt. List the contents of the Arena directory
+
+#!/bin/bash
+
+mkdir Arena
+cd Arena
+touch warrior.txt mage.txt archer.txt
+ls
 
